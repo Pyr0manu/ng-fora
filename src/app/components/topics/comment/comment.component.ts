afeed1e2d0c98bd9a3ded963f65ed8adbe7eb70a
Emmanuel <emmanuel.fabre.31@gmail.com>
2017-11-02 16:08:47 +0100

    comments

---
+++ src/app/components/topics/comment/comment.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Comment} from "../../../models/models";
+
+@Component({
+  selector: 'rc-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent implements OnInit {
+
+  @Input()comment:Comment;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getCommentUser(){
+    if (this.comment.user && this.comment.user.name){
+      return this.comment.user.name;
+    }
+    return "Anonymous";
+  }
+
+}
